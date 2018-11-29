c2f5be5e8682b629a9e539948dab119cc4dd9056
Pavankumar Kondeti <pkondeti@codeaurora.org>
2018-11-29 11:37:16 +0530

    sched: Introduce sched_busy_hysteresis_enable_cpus tunable
    Currently sched busy hysteresis feature is applied only for
    CPUs other than the min capacity CPUs. This policy restricts
    the flexibility on a system with more than 2 clusters. Add a
    tunable to specify which CPUs needs this feature. By default,
    the feature is turned off for all the CPUs.
    
    The usage of this tunable:
    
    echo 4-7 > /proc/sys/kernel/sched_busy_hysteresis_enable_cpus
    
    Change-Id: I636575af2c42e2774007582f3d589495c6a3a9f1
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -447,6 +447,7 @@ static struct ctl_table kern_table[] = {
 	},
 
 #endif
+#ifdef CONFIG_SMP
 	{
 		.procname	= "sched_upmigrate",
 		.data		= &sysctl_sched_capacity_margin_up,
@@ -461,6 +462,14 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= sched_updown_migrate_handler,
 	},
+	{
+		.procname	= "sched_busy_hysteresis_enable_cpus",
+		.data		= &sched_busy_hysteresis_cpubits,
+		.maxlen		= NR_CPUS,
+		.mode		= 0644,
+		.proc_handler	= proc_do_large_bitmap,
+	},
+#endif
 #ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname	= "sched_min_granularity_ns",
