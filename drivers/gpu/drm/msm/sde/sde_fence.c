d7004ff12dbb912b6abe52e0341e4668f6154ccd
Catinice <Catinice@outlook.com>
2021-04-23 12:37:09 +0000

    revert "drm/msm/sde: Use kmem_cache pool for struct sde_fence"
    Signed-off-by: Catinice <Catinice@outlook.com>

--- drivers/gpu/drm/msm/sde/sde_fence.c
+++ drivers/gpu/drm/msm/sde/sde_fence.c
@@ -19,8 +19,6 @@
 
 #define TIMELINE_VAL_LENGTH		128
 
-static struct kmem_cache *kmem_fence_pool;
-
 void *sde_sync_get(uint64_t fd)
 {
 	/* force signed compare, fdget accepts an int argument */
@@ -153,7 +151,7 @@ static void sde_fence_release(struct dma_fence *fence)
 	if (fence) {
 		f = to_sde_fence(fence);
 		kref_put(&f->ctx->kref, sde_fence_destroy);
-		kmem_cache_free(kmem_fence_pool, f);
+		kfree(f);
 	}
 }
 
@@ -201,13 +199,13 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val)
 	signed int fd = -EINVAL;
 	struct sde_fence_context *ctx = fence_ctx;
 
-	if (unlikely(!ctx)) {
+	if (!ctx) {
 		SDE_ERROR("invalid context\n");
 		goto exit;
 	}
 
-	sde_fence = kmem_cache_zalloc(kmem_fence_pool, GFP_KERNEL);
-	if (unlikely(!sde_fence))
+	sde_fence = kzalloc(sizeof(*sde_fence), GFP_KERNEL);
+	if (!sde_fence)
 		return -ENOMEM;
 
 	sde_fence->ctx = fence_ctx;
@@ -219,9 +217,7 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val)
 
 	/* create fd */
 	fd = get_unused_fd_flags(0);
-
-	if (unlikely(fd < 0)) {
-#ifdef CONFIG_FENCE_DEBUG
+	if (fd < 0) {
 		SDE_ERROR("failed to get_unused_fd_flags(), %s\n",
 							sde_fence->name);
 		dma_fence_put(&sde_fence->base);
@@ -230,7 +226,7 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val)
 
 	/* create fence */
 	sync_file = sync_file_create(&sde_fence->base);
-	if (unlikely(sync_file == NULL)) {
+	if (sync_file == NULL) {
 		put_unused_fd(fd);
 		fd = -EINVAL;
 		SDE_ERROR("couldn't create fence, %s\n", sde_fence->name);
@@ -265,7 +261,6 @@ struct sde_fence_context *sde_fence_init(const char *name, uint32_t drm_id)
 		return ERR_PTR(-ENOMEM);
 	}
 
-#ifdef CONFIG_FENCE_DEBUG
 	strlcpy(ctx->name, name, ARRAY_SIZE(ctx->name));
 	ctx->drm_id = drm_id;
 	kref_init(&ctx->kref);
@@ -347,7 +342,7 @@ int sde_fence_create(struct sde_fence_context *ctx, uint64_t *val,
 	int fd, rc = -EINVAL;
 	unsigned long flags;
 
-	if (unlikely(!ctx || !val)) {
+	if (!ctx || !val) {
 		SDE_ERROR("invalid argument(s), fence %d, pval %d\n",
 				ctx != NULL, val != NULL);
 		return rc;
@@ -373,7 +368,7 @@ int sde_fence_create(struct sde_fence_context *ctx, uint64_t *val,
 
 	SDE_EVT32(ctx->drm_id, trigger_value, fd);
 
-	if (likely(fd >= 0)) {
+	if (fd >= 0) {
 		rc = 0;
 		_sde_fence_trigger(ctx, ktime_get(), false);
 	} else {
@@ -505,11 +500,3 @@ void sde_debugfs_timeline_dump(struct sde_fence_context *ctx,
 	}
 	spin_unlock(&ctx->list_lock);
 }
-
-static int __init sde_kmem_pool_init(void)
-{
-	kmem_fence_pool = KMEM_CACHE(sde_fence, SLAB_HWCACHE_ALIGN | SLAB_PANIC);
-	return 0;
-}
-
-module_init(sde_kmem_pool_init);
